a120b1fbb99832aa6f7999306b90a13dd6523f1e
Justin Joyce <jjoyce07@yahoo.com>
2016-06-02 09:44:51 -0500

    added equals functionality

--- JavaScriptCalculator/script.js
+++ JavaScriptCalculator/script.js
@@ -18,11 +18,32 @@ $(document).ready(function(){
         totaldiv.text(number);
         testNumLength(number);
     });
-
-    $('#operators > a').not("#equals").click(function(){
+    $("#operators > a").not("#equals").click(function(){
         operator = $(this).text();
         newnumber = number;
         number = "";
         totaldiv.text("0");
+    });
+    $("#clear,#clearall").click(function(){
+        number = "";
+        totaldiv.text("0");
+        if ($(this).attr("id") === "clearall") {
+            newnumber = "";
+        }
+    });
+    $("#equals").click(function(){
+        if (operator === "+"){
+        number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+        } else if (operator === "-"){
+        number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+        } else if (operator === "/"){
+        number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+        } else if (operator === "*"){
+        number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+        }
+    totaldiv.text(number);
+    testNumLength(number);
+    number = "";
+    newnumber = "";
     });
 });
