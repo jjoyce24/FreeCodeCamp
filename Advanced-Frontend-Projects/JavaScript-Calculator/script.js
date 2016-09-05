f99a65cdb32c64b3cc7f9a0e03c0815ad3dc7dd1
Justin Joyce <jjoyce07@yahoo.com>
2016-09-05 13:59:02 -0500

    completely new js that is more dry

--- Advanced-Frontend-Projects/JavaScript-Calculator/script.js
+++ Advanced-Frontend-Projects/JavaScript-Calculator/script.js
@@ -1,77 +1,85 @@
-$(document).ready(function(){
-    var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
+// Get all the keys from document
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+// Add onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function(e) {
+        // Get the input and button values
+        var input = document.querySelector('.input');
+        var inputVal = input.innerHTML;
+        var btnVal = this.getAttribute("data-value");
+        
+        // Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+        // If clear key is pressed, erase everything
+        if(btnVal == 'C') {
+            input.innerHTML = '';
+            decimalAdded = false;
+        }
+        
+        // If eval key is pressed, calculate and display the result
+        else if(btnVal == '=') {
+            var equation = inputVal;
+            var lastChar = equation[equation.length - 1];
+            
+            // Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+            
+            // Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+            if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+                equation = equation.replace(/.$/, '');
+            
+            if(equation)
+                input.innerHTML = eval(equation);
+                
+            decimalAdded = false;
+        }
+        
+        // Basic functionality of the calculator is complete. But there are some problems like 
+        // 1. No two operators should be added consecutively.
+        // 2. The equation shouldn't start from an operator except minus
+        // 3. not more than 1 decimal should be there in a number
+        
+        // We'll fix these issues using some simple checks
+        
+        // indexOf works only in IE9+
+        else if(operators.indexOf(btnVal) > -1) {
+            // Operator is clicked
+            // Get the last character from the equation
+            var lastChar = inputVal[inputVal.length - 1];
+            
+            // Only add operator if input is not empty and there is no operator at the last
+            if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+                input.innerHTML += btnVal;
+            
+            // Allow minus if the string is empty
+            else if(inputVal == '' && btnVal == '-') 
+                input.innerHTML += btnVal;
+            
+            // Replace the last operator (if exists) with the newly pressed operator
+            if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+                // Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
+                input.innerHTML = inputVal.replace(/.$/, btnVal);
             }
-        } 
-    };
-    var number = "";
-    var newnumber = "";
-    var operator = "";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
-    $("#numbers > a").not("#clear,#clearall").click(function(){
-        number += $(this).text();
-        totaldiv.text(number);
-        testNumLength(number);
-    });
-    $("#operators > a, #side > a").not("#equals, #decimal").click(function(){
-        if ($(this).text() === "√"){
-            operator = "sqrt";
-            $("#equals").click();
-            return;
+            
+            decimalAdded =false;
         }
-        operator = $(this).text();
-        newnumber = number;
-        number = "";
-        totaldiv.text("0");
-    });
-    $("#clear,#clearall").click(function(){
-        number = "";
-        totaldiv.text("0");
-        if ($(this).attr("id") === "clearall") {
-            newnumber = "";
-        }
-    });
-    $("#decimal").click(function(){
-        var numOfDecs = 0;
-        for (i=0;i<number.length;i++){
-            if (number[i] === "."){
-            numOfDecs++;
+        
+        // Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
+        else if(btnVal == '.') {
+            if(!decimalAdded) {
+                input.innerHTML += btnVal;
+                decimalAdded = true;
             }
         }
-        if (numOfDecs > 0) {
-            number = number;
-            number = round(number,9);
-            totaldiv.text(number);
-        } else {
-            number += ".";
-            number = round(number,9);
-            totaldiv.text(number);
+        
+        // if any other key is pressed, just append it
+        else {
+            input.innerHTML += btnVal;
         }
-        testNumLength(number);
-    });
-    //Add your last .click() here!
-    $("#equals").click(function(){
-        if (operator === "+"){
-            number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-        } else if (operator === "-"){
-            number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
-        } else if (operator === "/"){
-            number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
-        } else if (operator === "*"){
-            number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
-        } else if (operator === "sqrt") {
-            number = Math.sqrt(parseFloat(number,10)).toString(10);
-        } else if (operator === "^") {
-            number = ((Math.pow(parseFloat(newnumber,10),parseFloat(number,10))).toString(10));
-        }
-        totaldiv.text(number);
-        testNumLength(number);
-        number = "";
-        newnumber = "";
-    });
-});
+        
+        // prevent page jumps
+        e.preventDefault();
+    } 
+}
