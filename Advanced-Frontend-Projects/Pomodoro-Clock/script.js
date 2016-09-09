914e361a8959c35b348725904e809f94ef2730d1
Justin Joyce <jjoyce07@yahoo.com>
2016-09-09 12:42:30 -0500

    add counter to have time go down and disapear buttons when session begins

--- Advanced-Frontend-Projects/Pomodoro-Clock/script.js
+++ Advanced-Frontend-Projects/Pomodoro-Clock/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-	var buzzer = $("#buzzer")[0];
+	var buzzer = $("#buzzer")[0]; 
 	var count = parseInt($("#num").html());
 	var breakTime = parseInt($("#breakNum").html());
 	$("#reset").hide();
+	$("#start").click(function(){
+		var counter = setInterval(timer, 1000);
+		function timer(){
+			// Hide variables
+			$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").hide();
+			$("#timeType").html("Tomato Time:")
+			count -= 1;
+			if (count === 0){
+				
+				clearInterval(counter);
+			}
+			$("#num").html(count)
+		}
+	})
 
 
 	$("#minus5Clock").click(function(){
