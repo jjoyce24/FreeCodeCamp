d8962210501d383d5b0d32d899f9b04bc1bf6e89
Justin Joyce <jjoyce07@yahoo.com>
2016-09-09 14:28:19 -0500

    finish countdown timer to hide and show time

--- Advanced-Frontend-Projects/Pomodoro-Clock/script.js
+++ Advanced-Frontend-Projects/Pomodoro-Clock/script.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 		function timer(){
 			// Hide variables
 			$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").hide();
+			$("#timeType").show();
 			$("#timeType").html("Tomato Time: ")
 			count -= 1;
 			if (count === 0){
@@ -19,15 +20,27 @@ $(document).ready(function(){
 			$("#num").html(count);
 
 			function breakTimer(){
+				$("#timeType").show();
 				$("#timeType").html("Break Time: ")
 				$("#breakNum").show();
 				breakTime -= 1;
 				if (breakTime === 0){
-					clearInterval(startBreak)
+					clearInterval(startBreak);
+					$("#reset").show();
+					$("#breakNum, #timeType").hide();
 				}
 				$("#breakNum").html(breakTime);
 			}
 		}
+	})
+
+	$("#reset").click(function(){
+		count = 25;
+		breakTime = 10;
+		$("#num").html(count);
+		$("#breakNum").html(breakTime);
+		$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2").show();
+		$("#reset, #timeType").hide();
 	})
 
 
